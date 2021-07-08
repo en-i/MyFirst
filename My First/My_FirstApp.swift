63df1145e62c202933a7da7c037de46d4edca290
en-i <templebond@icloud.com>
2021-07-08 23:12:52 +0900

    Initial Commit

---
+++ My First/My_FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FirstApp.swift
+//  My First
+//
+//  Created by terada enishi on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct My_FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
