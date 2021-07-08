0044f8085348d2aeac16d5967dbea74f58819a9a
en-i <templebond@icloud.com>
2021-07-09 02:30:34 +0900

    first

--- My First/ContentView.swift
+++ My First/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputText = "Hello World!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(outputText)
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                outputText = "HI, Swift!"
+                print("Debug\n" + outputText)
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"切り替えボタン"/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
 }
